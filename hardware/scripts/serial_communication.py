9ff9466fcda52a0b05bf131a5f7b1f779966cada
mrtkas <mart.kaasiku@gmail.com>
2018-09-15 14:09:11 +0300

    serial works

--- hardware/scripts/serial_communication.py
+++ hardware/scripts/serial_communication.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-
+import serial
 import rospy
 
 if __name__ == '__main__':
     rospy.init_node("serial_communication", anonymous=True)
 
-    rate = rospy.Rate(60)
+    ser = serial.Serial(
+        port='/dev/ttyACM0',
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.EIGHTBITS
+    )
+
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        print("Hello")
+        print(ser.isOpen())
         rate.sleep()
